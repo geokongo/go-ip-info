72aee98bf64714e76ac30f5007988ee4b60d10af
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2023-02-21 17:57:05 +0300

    Creating the main plugin file

---
+++ go-ip-info.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Go IP Info
+ * Plugin Description: This plugin uses the IP2Location API to display IP address information like country, state, city, longitude, latitude and ISP.
+ */
+
+ 
