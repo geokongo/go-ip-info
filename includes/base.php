145de06a8c92f07c56e59eb0082c0f5c59333459
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2023-02-23 14:06:49 +0300

    Updating the base class namespaces

--- includes/base.php
+++ includes/base.php
@@ -1,1 +1,19 @@
 <?php
+namespace GOIPInfo;
+
+/**
+ * @package GOIPInfo
+ */
+class Base{
+
+    public $plugin_path;
+    public $plugin_url;
+    public $plugin;
+
+    public function __construct(){
+        $this->plugin_path = plugin_dir_path(dirname(__FILE__, 1));
+        $this->plugin_url = plugin_dir_url(dirname(__FILE__, 1));
+        $this->plugin = plugin_basename(dirname(__FILE__, 2)) . '/go-ip-info.php';
+    }
+
+}
